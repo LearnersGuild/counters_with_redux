713675f30ef91b0226669184ce0a729e1460b825
neurodynamic <>
2018-04-23 13:02:04 -0700

    moves store code to store file

--- src/index.js
+++ src/index.js
@@ -3,26 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-
-
-const defaultState = { count: 0 }
-
-const reducer = function (state = defaultState, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return { ...state, count: state.count + 1 }
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer)
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux'
+
+const defaultState = { count: 0 }
+
+const reducer = function (state = defaultState, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+export default store
