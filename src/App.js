37b6e237c4a4b4cfd42990e371cc234ae29d5642
neurodynamic <>
2018-04-23 12:03:12 -0700

    converts to using Provider component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { createStore } from 'redux'
+import { connect } from 'react-redux'
 
-const defaultState = { count: 0 }
+const mapStateToProps = state => ({
+  count: state.count
+})
 
-const reducer  = function(state = defaultState, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return { ...state, count: state.count + 1 }
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer)
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick: () => dispatch({ type: 'INCREMENT' })
+})
 class App extends Component {
-  componentWillMount() {
-    store.subscribe(() => this.setState(store.getState()));
-    this.setState(store.getState());
-  }
-
   render() {
-    const onClick = () => store.dispatch({ type: 'INCREMENT' })
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p>{this.state.count}</p>
-        <button onClick={onClick}>+</button>
+        <p>{this.props.count}</p>
+        <button onClick={this.props.onClick}>+</button>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const defaultState = { count: 0 }
+
+const reducer = function (state = defaultState, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
