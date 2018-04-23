bab815a5c3d6f528c9eb21a990cbc2e6d2ece4b1
neurodynamic <>
2018-04-23 11:53:43 -0700

    adds redux-based counter by explicitly subscribing to store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux'
 
+const defaultState = { count: 0 }
+
+const reducer  = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
 class App extends Component {
+  componentWillMount() {
+    store.subscribe(() => this.setState(store.getState()));
+    this.setState(store.getState());
+  }
+
   render() {
+    const onClick = () => store.dispatch({ type: 'INCREMENT' })
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{this.state.count}</p>
+        <button onClick={onClick}>+</button>
       </div>
     );
   }
